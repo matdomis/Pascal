90c604d5b361a02972fc4f4c37bf670f5f0a11a1
Matheus Spiazzi Domingues <35573712+matheusdomis@users.noreply.github.com>
2019-06-26 10:57:01 -0300

    selection sort
    https://codepumpkin.com/wp-content/uploads/2017/10/selectionSort.gif

--- sort and search/selectionsort.pas
+++ sort and search/selectionsort.pas
@@ -1,40 +1,7 @@
-program sort;
+{=================================================================================}
+{                      ୧☉□☉୨         SELECTION         ୧☉□☉୨                    }
+{=================================================================================}
 
-type
-    array_inteiros = array [1..100] of integer;
-function tamarr (V: array_inteiros): integer;
-var
-    k: integer;
-begin
-    k := 1;
-    while V[k] <> 0 do
-        k := k + 1;
-    tamarr := k - 1;
-end;
-procedure generate_array (var V: array_inteiros);
-var
-    i, num: integer;
-begin
-    read(num);
-    i := 0;
-    while num <> 0 do
-    begin
-        i := i + 1;
-        V[i] := num;
-        read(num);
-    end;
-end;
-    
-//
-procedure print_array (V: array_inteiros);
-var
-    k, tam: integer;
-begin
-    tam := tamarr(V);
-    for k := 1 to tam do
-        write(V[k], ' ');
-end;
-//
 
 procedure selection_sort (var V: array_inteiros);
 var
@@ -59,26 +26,3 @@ begin
         V[i] := arm_valor;
     end;
 end;
-                
-          
-          
-
-
-//
-var
-    vetor: array_inteiros;
-begin
-    generate_array(vetor);
-    selection_sort(vetor);
-    print_array(vetor);
-end.
-
-
-
-
-
-
-
-
-
-
