b7faaa9bf989c8318fde15431dd54494997d40b1
Matheus Spiazzi Domingues <35573712+matheusdomis@users.noreply.github.com>
2019-06-26 14:58:13 -0300

    compara lista

--- farma_alg/Lista 5/comparalistas.pas
+++ farma_alg/Lista 5/comparalistas.pas
@@ -1,4 +1,31 @@
-program listainversa;
+{ 
+
+Faça um programa em Free Pascal que leia duas listas de valores inteiros positivos, cada um deles terminado em zero. O zero indica o término da entrada de dados e não deve ser processado. Seu programa deve inicialmente testar se as duas listas têm o mesmo número de elementos. Se não tiverem deve imprimir a mensagem "Tamanhos diferentes". Caso tenham o mesmo tamanho, seu programa deve verificar se um conjunto é o inverso do outro. Se forem, imprimir "Sim" e em caso contrário, imprimir "Nao"
+
+Exemplo de entrada 1:
+1 2 3 4 5 6 0
+9 8 7 6 5 4 3 2 1 0
+
+Saída esperada:
+Tamanhos diferentes
+
+Exemplo de entrada 2:
+1 2 3 4 5 6 0
+6 5 4 3 2 1 0
+
+Saída esperada:
+Sim
+
+Exemplo de entrada 3:
+1 7 3 3 5 6 0
+6 5 4 3 2 1 0
+
+Saída esperada:
+Nao
+
+}
+
+program comparalista;
 type
     array_inteiros = array [1..100] of integer;
 
