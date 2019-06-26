9af31faa632a52e0aea0f99de42238faffb982f2
Matheus Spiazzi Domingues <35573712+matheusdomis@users.noreply.github.com>
2019-06-26 11:47:46 -0300

    compara listas

---
+++ farma_alg/Lista 5/comparalistas.pas
@@ -0,0 +1,82 @@
+program listainversa;
+type
+    array_inteiros = array [1..100] of integer;
+
+{*********************************************************************************}
+{                           FUNCTIONS                                             }
+{*********************************************************************************}
+
+procedure generate_array(var v: array_inteiros);
+var
+	k, num: integer;
+begin
+	k := 1;
+	read(num);
+	while num <> 0 do
+	begin
+		v[k] := num;
+		read(num);
+		k := k + 1;
+	end;
+end;
+
+function tamarr(V: array_inteiros): integer;
+var
+ k: integer;
+begin
+  k := 1;
+  while V[k] <> 0 do
+   k := k + 1;
+  tamarr := k - 1;
+end;
+
+procedure print_array(V: array_inteiros; N: integer);
+var
+	i: integer;
+begin
+	for i := 1 to N do
+		write(V[i], ' ');
+end;
+
+procedure reverse_array (var V: array_inteiros; N: integer);
+var
+    temparr: array_inteiros;
+    i, k: integer;
+begin
+    temparr := V;
+    i := 1;
+    for k := N downto 1 do
+    begin
+        V[i] := temparr[k];
+        i := i + 1;
+    end;
+end;
+
+{*********************************************************************************}
+{                           MAIN                                                  }
+{*********************************************************************************}
+
+var
+    vetor1, vetor2: array_inteiros;
+    i, tam1, tam2: integer;
+    b: boolean;
+begin
+    generate_array(vetor1);
+    generate_array(vetor2);
+    tam1 := tamarr(vetor1);
+    tam2 := tamarr(vetor2);
+    if tam1 = tam2 then
+    begin
+        reverse_array(vetor1, tam1);
+        b := True;
+        for i := 1 to tam1 do
+            if vetor1[i] <> vetor2[i] then
+                b := False;
+        if b then
+            writeln('Sim')
+        else
+            writeln('Nao');
+    end
+    else
+        writeln('Tamanhos diferentes');
+end.
