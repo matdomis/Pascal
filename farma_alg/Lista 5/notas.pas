bd1048eccc269089af24b8d8aa100d708aa307f3
Matheus Spiazzi Domingues <35573712+matheusdomis@users.noreply.github.com>
2019-06-26 14:55:42 -0300

    notas

--- farma_alg/Lista 5/notas.pas
+++ farma_alg/Lista 5/notas.pas
@@ -1,3 +1,23 @@
+{ 
+
+Considere que um professor tem N alunos em sua turma de Algoritmos e Estruturas de Dados I. Este professor calcula a média final dos alunos após a aplicação de três provas Prova1, Prova2 e Prova3 com pesos para as provas respectivamente de 1, 2 e 3. A média final é o resultado da média ponderada (Prova1 + 2 x Prova2 + 3 x Prova3)/6.
+
+Faça um programa em Free Pascal que leia N, 0 <= N <= 100 do teclado (não precisa testar se o valor lido está fora desta faixa) e em seguida leia três listas de notas para os N alunos. As notas sempre são de 0 a 100, não precisa testar isso. Considere que no lugar dos nomes os alunos são numerados de 1 até N. Seu programa deve imprimir a média final dos N alunos (com duas casas decimais) e, para cada aluno, indicar se foi aprovado (média maior ou igual a 70), se deve fazer a prova final (média maior ou igual a 40 e menor que 70) ou se foi reprovado (média inferior a 40).
+
+Exemplo de entrada: // Observacões
+5 // a turma tem 5 alunos
+50 30 10 80 85 // notas da prova 1 para os 5 alunos
+40 40 60 55 75 // notas da prova 2 para os 5 alunos
+80 35 90 20 95 // notas da prova 3 para os 5 alunos
+
+Saída esperada:
+Aluno 1: 61.67 Final
+Aluno 2: 35.83 Reprovado
+Aluno 3: 66.67 Final
+Aluno 4: 41.67 Final
+Aluno 5: 86.67 Aprovado
+}
+
 program code;
 const
    MIN = 1;
