5e0381a2982dee6e4542c9fb453ff63566343431
Matheus Spiazzi Domingues <35573712+matheusdomis@users.noreply.github.com>
2019-08-11 20:26:42 -0300

    dificil a vida

--- sort and search/mergesort.pas
+++ sort and search/mergesort.pas
@@ -1,40 +1,5 @@
-program sort;
+=S
 
-type
-    array_inteiros = array [1..100] of integer;
-function tamarr (V: array_inteiros): integer;
-var
-    k: integer;
-begin
-    k := 1;
-    while V[k] <> 0 do
-        k := k + 1;
-    tamarr := k - 1;
-end;
-procedure generate_array (var V: array_inteiros);
-var
-    i, num: integer;
-begin
-    read(num);
-    i := 0;
-    while num <> 0 do
-    begin
-        i := i + 1;
-        V[i] := num;
-        read(num);
-    end;
-end;
-    
-//
-procedure print_array (V: array_inteiros);
-var
-    k, tam: integer;
-begin
-    tam := tamarr(V);
-    for k := 1 to tam do
-        write(V[k], ' ');
-end;
-//
 
 procedure merge_sort(var V: array_inteiros);
 var
@@ -69,23 +34,3 @@ begin
     end;
 
 end;
-    
-
-
-
-
-
-
-
-
-
-
-
-
-//
-var
-    vetor: array_inteiros;
-begin
-    generate_array(vetor);
-    merge_sort(vetor);
-end.
